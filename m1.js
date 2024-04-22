4ad567434700c5e66d08f7d32e359b3c68575112
ANUSURI-MANIKANTA <167809090+ANUSURI-MANIKANTA@users.noreply.github.com>
2024-04-23 00:16:17 +0530

    Add files via upload

---
+++ m1.js
@@ -0,0 +1,7 @@
+let profile= document.querySelector('.header .flex .profile');
+document.querySelector('#user-btn').onclick = () =>{
+    profile.classList.toggle('active');
+}
+window.onscroll = () =>{
+    profile.classList.toggle('remove');
+}
